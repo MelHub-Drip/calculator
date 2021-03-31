fcbbd213d262ab01cbfd5eb07b99d9fe856b441d
Melven <239booysen@gmail.com>
2021-03-31 08:04:44 +0200

    First commit

---
+++ src/za/ca/cput/calculator/Division.java
@@ -0,0 +1,16 @@
+package za.ca.cput.calculator;
+
+/*
+*Return result of integer division
+*
+ */
+public class Division
+{
+    public static int intergerDivision (int numerator, int denominator)
+    {
+        if (denominator < 1) return 0;
+        return numerator / denominator;
+    }
+
+
+}
